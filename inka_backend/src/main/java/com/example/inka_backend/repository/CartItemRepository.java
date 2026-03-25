c855a38a513d983e2a3815f3f8233a0503cfc09c
HirunaWeerakoon <it24610788@my.sliit.lk>
2026-03-25 13:33:54 +0530

    cart merged

--- inka_backend/src/main/java/com/example/inka_backend/repository/CartItemRepository.java
+++ inka_backend/src/main/java/com/example/inka_backend/repository/CartItemRepository.java
@@ -3,14 +3,17 @@ package com.example.inka_backend.repository;
 import com.example.inka_backend.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
-    List<CartItem> findByCustomerId(Long customerId);
+    List<CartItem> findByCustomer_CustomerId(Long customerId);
 
-    Optional<CartItem> findByCustomerIdAndProductId(Long customerId, Long productId);
+    Optional<CartItem> findByCustomer_CustomerIdAndProduct_ProductId(Long customerId, Long productId);
 
-    void deleteByCustomerId(Long customerId);
+    void deleteByCustomer_CustomerId(Long customerId);
 }
